5e78c25781d9cc20166c8f0267a863d01864a94f
陈翀 <2046917227@qq.com>
2019-01-22 10:40:31 +0800

    更改优化

--- pages/position/city/index.js
+++ pages/position/city/index.js
@@ -5,17 +5,16 @@ Page({
     hidden: true,
     city,
   },
-  onReady() {
-    let that = this;
-    wx.createSelectorQuery().select('.indexBar-box').boundingClientRect(res=>{
-      that.setData({
-        boxTop: res.top
-      })
+  getBoxTop() {
+    wx.createSelectorQuery()
+    .select('.indexBar-box')
+    .boundingClientRect(res=>{
+      this.setData({ boxTop: res.top })
     }).exec();
   },
-
-  onLoad: function (options) {
+  onLoad(options) {
     this.initChar();
+    this.getBoxTop();
   },
   initChar(){
     const not_show = ['I', 'O', 'U', 'V'];
@@ -24,69 +23,55 @@ Page({
       const charCode = String.fromCharCode(65 + i);
       list[i] = String.fromCharCode(65 + i)
     }
-    for (let i in not_show) {
-      if (list.includes(not_show[i])) {
-        const index = list.indexOf(not_show[i])
-        list.splice(index, 1)
-      }
-    }
+    list = list.filter(item=>{
+      return not_show.indexOf(item) == -1
+    })
     this.setData({ list, listCur: list[0] })
   },
-
   //滑动选择Item
   tMove(e) {
-    let y = e.touches[0].clientY,
-      offsettop = this.data.boxTop;
-    const { list } = this.data;
+    let y = e.touches[0].clientY;
+    const { boxTop, list } = this.data;
     //判断选择区域,只有在选择区才会生效
-    if (y > offsettop) {
-      let num = parseInt((y - offsettop) / 20);
+    if (y > boxTop) {
+      let num = parseInt((y - boxTop) / 20);
       const listCur = list[num] ? list[num] : list[list.length - 1];
       this.setData({ listCur })
     };
   },
-
-  //触发全部开始选择
   tStart() {
     this.setData({
       hidden: false
     })
   },
-
-  //触发结束选择
   tEnd() {
     this.setData({
-      hidden: true,
-      listCurID: this.data.listCur
+      hidden: true
     })
   },
-
   getCur(e) {
     this.setData({
       hidden: false,
       listCur: this.data.list[e.target.id],
     })
   },
-
   setCur(e) {
     this.setData({
       hidden: true,
       listCur: this.data.listCur
     })
   },
-
   //搜索回车键
   searchConfirm(e){
     const { value } = e.detail;
-    console.log(value)
-    let key;
+    let listCur;
     for(let i of city){
       for(let j of i.item){
         if( j.name.includes(value)){
-          key = j.key
+          listCur = j.key
         }
       }
     }
-    this.setData({ listCur : key })
+    this.setData({ listCur })
   }
 })
